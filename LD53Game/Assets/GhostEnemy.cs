2954527b98f4f8c4057cc893c41f8019a104489d
JakobRW <jakobrw@outlook.com>
2023-04-30 17:26:03 +0200

    Push

---
+++ LD53Game/Assets/GhostEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostEnemy : MonoBehaviour
+{
+    public Collider2D collider2D;
+    public PlayerController player;
+    public Animator squishAnimator;
+
+    void OnTriggerEnter2D(Collider2D otherCollider) {
+        if (otherCollider.tag == "PlayerAttack") {
+            TakeDamage();
+        }
+    }
+
+    public void TakeDamage() {
+        player.DashAttackHit();
+        squishAnimator.SetTrigger("Squish");
+
+        collider2D.enabled = false;
+        StartCoroutine(ToggleCollider());
+    }
+
+    IEnumerator ToggleCollider() {
+        yield return new WaitForSeconds(0.1f);
+        collider2D.enabled = true;
+    }
+}
--- LD53Game/Assets/Scripts/Enemy.cs
+++ LD53Game/Assets/Scripts/Enemy.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public virtual void TakeDamage() {
-        print("Damage");
     }
 }
